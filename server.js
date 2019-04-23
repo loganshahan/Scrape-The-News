962d65e5464e91baf2338a5ab8f2cc88b0122cd5
Logan Shahan <loganshahan88@gmail.com>
2019-04-23 12:03:47 -0400

    adding files to folders

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
+var express = require("express");
+var bodyParser = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var exphbs = require("express-handlebars");
 
+var routes = require("./controllers")
+var db = require("./models");
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
